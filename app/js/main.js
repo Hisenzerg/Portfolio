edd540b52e9403d5a026952887698cff06451d00
Hisenzerg <mrhisenberg100@gmail.com>
2021-06-24 13:12:34 -0400

    fixed dist

--- app/js/main.js
+++ app/js/main.js
@@ -9,4 +9,20 @@ $(function () {
     $('.menu').removeClass('menu--active')
   })
 
+  $(".contact__form").submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function () {
+      alert("Thank you!");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
+
 });
